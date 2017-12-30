6bdfffb0a445f8bc5c60e0685b7722ee340d9372
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-12-30 09:38:59 -0700

    add searchbox for movies

--- client/src/list/Movies.js
+++ client/src/list/Movies.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Searchbox from '../search/Searchbox';
 let axios = require('axios');
 
 export default class MovieList extends Component {
@@ -8,11 +9,13 @@ export default class MovieList extends Component {
       initialized: false,
       data: null
     }
+    this.updateResults = this.updateResults.bind(this);
+    this.getMovieList = this.getMovieList.bind(this);
   }
 
-  getMovieList() {
+  getMovieList(q) {
     return new Promise((resolve, reject) => {
-      axios.post('/results', {query: "lion"}).then((res) => {
+      axios.post('/results', {query: q}).then((res) => {
         this.setState({
           data: res.data,
           initialized: true
@@ -22,7 +25,10 @@ export default class MovieList extends Component {
     })
   }
  
-
+  updateResults(query) {
+    console.log(query);
+    this.getMovieList(query);
+  }
   componentDidMount() {
     if (!(this.state.initialized)) {
       this.getMovieList();
@@ -31,13 +37,19 @@ export default class MovieList extends Component {
   render() {
    let listHtml = [];
    if (this.state.initialized) {
-     this.state.data.results.forEach((e, i)=> {
-       listHtml.push(<li key={"title"+i}>{e.title}</li>)
-     })
+     if (this.state.data.results) {
+      this.state.data.results.forEach((e, i)=> {
+        listHtml.push(<li key={"title"+i}>{e.title}</li>)
+      })
+    } else {
+      listHtml.push(<li>No results</li>)
+    }
 
       return (
         <div>
           <h1>Movies</h1>
+
+          <Searchbox label="Search movies" submit={this.updateResults}/>
           <ul>{listHtml}</ul>
         </div>
       );
---
+++ client/src/search/Searchbox.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Message } from 'semantic-ui-react'
+
+export default class Searchbox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      error: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    console.log(e.target.value);
+    if (e.target.value) {
+      this.setState({
+        query: e.target.value,
+        error: ''
+      })
+    } else {
+      console.log('error)')
+      this.setState({
+        query: '',
+        error: "Input must not be empty"
+      })
+    }
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.query) {
+      this.props.submit(this.state.query);
+    } else {
+      this.setState({
+        error: "Input must not be empty"
+      })
+    }
+  }
+  render() {
+    let label = (this.props.label) ? this.props.label : "Search";
+    let messageList = [];
+  
+    if (this.state.error != '') {
+      messageList.push(<Message
+        error
+        list={[this.state.error]}
+      />)
+    } else {
+      messageList = []
+    }
+    return (
+      <form>
+        <label htmlFor="search">{label}</label>
+        <input name="search" value={this.state.query} id="search" onChange={this.handleChange} />
+        <input type="submit" onClick={this.handleSubmit}/> 
+        {messageList}
+        
+      </form>
+    );
+  }
+}
